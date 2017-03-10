c1faeeee4bb2fd55d473e00aa3082cdc59be0224
Itay Asayag <itay@asayag.ddns.net>
2017-03-10 20:37:40 +0000

    Improve uptime data gathering.

--- getdata.php
+++ getdata.php
@@ -15,6 +15,7 @@
         $ipexternal = shell_exec("dig +short myip.opendns.com @resolver1.opendns.com");
         str_replace("\n", "", $ipexternal);
 
+        /* not effective 
 
         $uptime = shell_exec("uptime -p");
         $uptime = str_replace("up ", "", $uptime);
@@ -23,6 +24,16 @@
 
         $uptime = str_replace(" minutes", "|", $uptime);
         $uptime = str_replace(" minute", "|", $uptime);
+        
+        */
+        function secondsToTime($inputSeconds) {
+  $then = new DateTime(date('Y-m-d H:i:s', $inputSeconds));
+  $now = new DateTime(date('Y-m-d H:i:s', time()));
+  $diff = $then->diff($now);
+  return array('years' => $diff->y, 'months' => $diff->m, 'days' => $diff->d, 'hours' => $diff->h, 'minutes' => $diff->i, 'seconds' => $diff->s);
+}
+
+$uptime = (secondsToTime(time() - floor(shell_exec("tail /proc/uptime"))));
 
         $used = shell_exec("df -H --total --output=used");
         $used = preg_split ('/$\R?^/m', $used);
@@ -33,16 +44,18 @@
         $total = $total[count($total)-1];
 
 $data = array(
-    $proc_temp,
+    $proc_temp, 
     $ipinternal,
     $ipexternal,
-    preg_split("/[|]/m",$uptime),
+    $uptime,
     $used,
     $total
 );
+
+$data = json_encode($data);
 $data = str_replace(" \\n","",$data);
-$data = str_replace("\\n","",json_encode($data));
+$data = str_replace("\\n","",$data);
 $data = str_replace(" \\n","",$data);
 $data = str_replace("G","",$data);
 
-echo $data;
+echo($data);
